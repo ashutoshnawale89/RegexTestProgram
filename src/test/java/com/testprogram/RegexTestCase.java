d8bd88b3b5b9bbfb7eee26271f002f1bbf71ccd3
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-28 11:11:48 +0530

    Added:All EmailId Test Cases

--- src/test/java/com/testprogram/RegexTestCase.java
+++ src/test/java/com/testprogram/RegexTestCase.java
@@ -6,38 +6,155 @@ import org.junit.Test;
 import com.program.RegexProgram;
 
 public class RegexTestCase {
+
+
+
 	@Test
-	   public void firstNameTest() {
-		   RegexProgram obj=new RegexProgram();
-		   String regex="^[A-Z]+[a-z]{2,}";
-		   String user="Ashu";
-		   Assert.assertEquals(true, obj.regexRunner(regex, user));
-	   }
-		@Test
-		   public void lastNameTest() {
-			   RegexProgram obj=new RegexProgram();
-			   String regex="^[A-Z]{1}[A-Za-z]{2,}";
-			   String user="Nawale";
-			   Assert.assertEquals(true, obj.regexRunner(regex, user));
-		   }
-		@Test
-		   public void mobileNumberTest() {
-			   RegexProgram obj=new RegexProgram();
-			   String regex="^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
-			   String user="91 7776042800";
-			   Assert.assertEquals(true, obj.regexRunner(regex, user));
-		   }
-		@Test
-		   public void emailIDTest() {
-			   RegexProgram obj=new RegexProgram();
-			   String regex="^[a-z]{3}[.]?[a-z]?[@#$&]{1}[a-z]{2}[.]{1}[a-z]{2}[.]{1}[a-z]{2}$";
-			   String user="abc@bi.co.in";
-			   Assert.assertEquals(true, obj.regexRunner(regex, user));
-		   }
-		public void passwrdTest() {
-			   RegexProgram obj=new RegexProgram();
-			   String regex="^[A-Z]+[A-Za-z]{7,}[@#$&][1][0-9]+$";
-			   String user="Password@12345";
-			   Assert.assertEquals(true, obj.regexRunner(regex, user));
-		   }
+	public void emailIdTest1() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc@yahoo.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
 	}
+	@Test
+	public void emailIdTest2() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc.100@yahoo.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest3() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc111@abc.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest4() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc-100@abc.net";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest5() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc.100@abc.com.au";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest6() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc@1.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+
+	@Test
+	public void emailIdTest7() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc@gmail.com.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}@Test
+	public void emailIdTest8() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc+100@gmail.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}@Test
+	public void emailIdTest9() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest10() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc@.com.my";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest11() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc123@gmail.a";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest12() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="Password@12345";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest13() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc123@.com.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest14() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user=".abc@abc.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest15() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc()*@gmail.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+
+	@Test
+	public void emailIdTest16() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc@%*.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest17() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc..2002@gmail.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest18() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc.@gmail.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest19() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc@abc@gmail.com";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest20() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc@gmail.com.1a";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+	@Test
+	public void emailIdTest21() {
+		RegexProgram obj=new RegexProgram();
+		String regex="^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$";
+		String user="abc@gmail.com.aa.au";
+		Assert.assertEquals(true, obj.regexRunner(regex, user));
+	}
+
+}
